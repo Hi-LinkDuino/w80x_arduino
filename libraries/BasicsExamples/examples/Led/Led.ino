71902815f4f0eb98f203b9746131d49a3fe31a63
宋志硕 <81579469@qq.com>
2022-03-14 22:23:12 +0800

    add Demo .ino file - add blink led demo & adc demo

--- libraries/BasicsExamples/examples/Led/Led.ino
+++ libraries/BasicsExamples/examples/Led/Led.ino
@@ -1,46 +1,17 @@
 #include <stdio.h>
 #include <Arduino.h>
-//#include "wm_hal.h"
+int led = PB0;
 
-static volatile uint8_t key_flag = 0;
-
-static void GPIO_Init(void)
-{
-    GPIO_InitTypeDef GPIO_InitStruct = {0};
-    GPIO_InitStruct.Pin = GPIO_PIN_0 | GPIO_PIN_1 | GPIO_PIN_2;
-    GPIO_InitStruct.Mode = GPIO_MODE_OUTPUT;
-    GPIO_InitStruct.Pull = GPIO_NOPULL;
-    HAL_GPIO_Init(GPIOB, &GPIO_InitStruct);
-    HAL_GPIO_WritePin(GPIOB, GPIO_PIN_0 | GPIO_PIN_1 | GPIO_PIN_2, GPIO_PIN_SET);
-}
-
+// the setup routine runs once when you press reset:
 void setup() {
-  // put your setup code here, to run once:
-    printf("Led Demo\r\n");
-    GPIO_Init();
+  // initialize the digital pin as an output.
+  pinMode(led, OUTPUT);
 }
 
+// the loop routine runs over and over again forever:
 void loop() {
-   // put your main code here, to run repeatedly:
-    HAL_Delay(500);
-    key_flag++;
-    
-    if (key_flag % 3 == 0)
-    {
-        HAL_GPIO_WritePin(GPIOB, GPIO_PIN_0, (GPIO_PinState)0);
-        HAL_GPIO_WritePin(GPIOB, GPIO_PIN_1, (GPIO_PinState)1);
-        HAL_GPIO_WritePin(GPIOB, GPIO_PIN_2, (GPIO_PinState)1);
-    }
-    else if (key_flag % 3 == 1)
-    {
-        HAL_GPIO_WritePin(GPIOB, GPIO_PIN_0, (GPIO_PinState)1);
-        HAL_GPIO_WritePin(GPIOB, GPIO_PIN_1, (GPIO_PinState)0);
-        HAL_GPIO_WritePin(GPIOB, GPIO_PIN_2, (GPIO_PinState)1);
-    }
-    else
-    {
-        HAL_GPIO_WritePin(GPIOB, GPIO_PIN_0, (GPIO_PinState)1);
-        HAL_GPIO_WritePin(GPIOB, GPIO_PIN_1, (GPIO_PinState)1);
-        HAL_GPIO_WritePin(GPIOB, GPIO_PIN_2, (GPIO_PinState)0);
-    }
+  digitalWrite(led, HIGH);   // turn the LED on (HIGH is the voltage level)
+  delay(1000);               // wait for a second
+  digitalWrite(led, LOW);    // turn the LED off by making the voltage LOW
+  delay(1000);               // wait for a second
 }
